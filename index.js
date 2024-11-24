2431d0cacd827b2d8879d62cbc7a51fb39e5fa8d
Tilak Patel <tilakny@gmail.com>
2024-11-24 06:35:53 -0500

    added change

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.REMOTE_SERVER,
+    domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
 app.use(session(sessionOptions));
